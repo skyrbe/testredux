8b2ff6f0f81656be1d92d71a88e5e5c93a2bf9d3
Harsha Venkatram <harsha.venkatram@sakhatech.com>
2018-07-21 17:48:12 +0530

    check precommit

---
+++ src/components/users/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
+
+const UserList = ({ data, onDelete }) => {
+  function createTableBody() {
+    return _.map(data, (user) => {
+      return (
+        <tr key={user.id}>
+          <td>
+            {user.name}
+          </td>
+          <td>
+            {user.location}
+          </td>
+          <td>
+            <Link to={`/users/${user.id}`}>
+              Edit
+            </Link>
+            <input
+              type="button"
+              value="delete"
+              className="btn btn-primary"
+              onClick={() => onDelete(user.id)}
+            />
+          </td>
+        </tr>
+      );
+    });
+  }
+  return (
+    <table className="striped bordered condensed hover">
+      <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Location
+          </th>
+          <th>
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {createTableBody()}
+      </tbody>
+    </table>);
+};
+
+export default UserList;
---
+++ src/containers/activeUser/index.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import CreateUserForm from '../../components/activeUser';
+
+import {
+  getUser
+} from '../../reducers/users';
+
+class ActiveUser extends Component {
+  componentDidMount() {
+    const url = this.props.location.pathname;
+    const splitArray = url.split('/');
+    this.setState({ id: splitArray[splitArray.length - 1] }, () => {
+      this.props.getUser(this.state.id);
+    });
+  }
+
+
+  submit = (values) => {
+    // console.log(values);
+  }
+
+  render() {
+    const { loading, loaded, activeUser } = this.props;
+    return (
+      <div>
+        <h1>
+          Home
+        </h1>
+        { loading && (
+          <div>
+            Loading
+          </div>
+        )}
+        { loaded && activeUser && (
+          <CreateUserForm
+            onSubmit={this.submit}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  activeUser: state.users.activeUser,
+  loading: state.users.loading,
+  loaded: state.users.loaded
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getUser,
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActiveUser);
